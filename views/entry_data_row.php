b210bbf74d19cd5824701335a4a4c0601831cf31
Mike Kruk <tamagokun@gmail.com>
2014-04-30 07:14:36 -0400

    arrays should be imploded so we can see values

--- views/entry_data_row.php
+++ views/entry_data_row.php
@@ -1,3 +1,10 @@
 <?php foreach ($fields as $field_name => $field): ?>
-    <p><strong><?php echo $field['label']; ?>:</strong> <?php echo $field['value']; ?></p>
+    <p>
+        <strong><?php echo $field['label']; ?>:</strong>
+        <?php if (is_array($field['value'])): ?>
+            <?php echo implode(', ', $field['value']); ?>
+        <?php else: ?>
+            <?php echo $field['value']; ?>
+        <?php endif; ?>
+    </p>
 <?php endforeach; ?>

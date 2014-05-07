2344110d44fcf42d50d6024ca2d16df280fe1f3e
Mike Kruk <tamagokun@gmail.com>
2014-05-07 14:29:42 -0400

    bump version

--- acf-forms.php
+++ acf-forms.php
@@ -3,7 +3,7 @@
 Plugin Name: ACF Forms
 Plugin URI: https://github.com/tamagokun/acf-forms
 Description: Create forms using Advanced Custom Fields.
-Version: 0.1
+Version: 0.2
 Author: Mike Kruk
 Author URI: http://ripeworks.com/
 */
